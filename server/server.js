1b8058f90d29d44f6585640edb712ff8964b0953
hannakr <hkruotsalainen@gmail.com>
2018-03-05 20:25:42 -0800

    Add GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -28,6 +29,29 @@ app.get('/todos', (req, res) => {
     // then you can add stuff to it
   }, (err) => {
     res.status(400).send(err);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }, (err) => {
+    // this would catch any errors in the findById call (e.g. if the db goes away)
+    // but not errors in how we handle the result of that call
+    res.status(400).send();
+  }).catch((e) => {
+    // this catches anything that goes wrong in the above code
+    // use this instead of the above option
+    res.status(401).send();
   });
 });
 
